7124e1641cc2cc0bcd53fe74bba6081151159e76
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-10-08 23:23:49 -0700

    connecting css static files

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
+//requiring json data
 const { projects } = require("./data.json");
 
+//setting view engine to pug
 app.set("view engine", "pug");
+
+//connecting static css files
+app.use("/static", express.static("public"));
 
 //testing server
 app.get("/", (req, res) => {
