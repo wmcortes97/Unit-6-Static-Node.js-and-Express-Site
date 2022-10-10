2b976a1ada858d3ea6f3df598b4dbd79fcf7d320
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-10-10 16:07:53 -0700

    updated error pug pages

--- app.js
+++ app.js
@@ -39,7 +39,11 @@ app.use((req, res, next) => {
 //custom error handler
 app.use((err, req, res, next) => {
   res.locals.error = err;
-  res.render("error");
+  if (err.status === 404) {
+    res.render("page-not-found");
+  } else {
+    res.render("error");
+  }
 });
 
 app.listen(3000, () => {
