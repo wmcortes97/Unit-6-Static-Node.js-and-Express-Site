a1accbe91c37d0043718d9be7ce6c87c922b46d5
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-10-10 14:28:10 -0700

    refactored index route

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("view engine", "pug");
 //connecting static css files
 app.use("/static", express.static("public"));
 
-//index/ home route
+//home route
 app.get("/", (req, res) => {
   res.render("index", { projects });
 });
@@ -21,26 +21,12 @@ app.get("/about", (req, res) => {
 });
 
 //project route for each project
-app.get("/project/:id", (req, res) => {
-  const { id } = req.params; // const id = req.params.id
-  const { project_name } = projects[id]; // const project_name = projects[0].project_name
-  const { description } = projects[id];
-  const { technologies } = projects[id]; //returns an array
-  const { live_link } = projects[id];
-  const { github_link } = projects[id];
-  const { image_urls } = projects[id];
-
-  //console.log(projects[1].image_urls[0]);
-
-  const focusedProjectInfo = {
-    id,
-    project_name,
-    description,
-    technologies,
-    live_link,
-    github_link,
-  };
-  res.render("project", focusedProjectInfo);
+app.get("/project/:id", (req, res, next) => {
+  if (projects[req.params.id]) {
+    res.render("project", { project: projects[req.params.id] });
+  } else {
+    next();
+  }
 });
 
 //404 error object
